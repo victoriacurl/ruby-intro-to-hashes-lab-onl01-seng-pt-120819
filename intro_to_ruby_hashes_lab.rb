4f1a7fcf99e9310ab98b84cb5c8857f9e6f39823
Victoria Curl <victoriacurl9@gmail.com>
2019-12-20 19:17:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,9 @@ def monopoly_with_third_tier
 	 :railroads =>
 	  {
 		  :pieces => 4,
-			:names => {},
+			:names => {
+				:reading_railroad =>{}
+			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
